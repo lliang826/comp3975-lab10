30f642ed9f6810c4008cafa460410f3bd6802572
Lawrence Liang <[xlawrence.liangx@gmail.com]>
2022-03-10 21:25:54 -0800

    fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => (
     <React.Fragment>
     <footer class="border-top footer text-muted" style={{position: "absolute", bottom: 0, width: "100%", whiteSpace: "nowrap", lineHeight: "60px"}}>
         <div class="container">
-            <p>© 2022 - FizzBuzzWeb - <a href="/Home/Privacy">Privacy</a></p>
+            <p>© 2022 - FizzBuzzWeb - <Link to="/Home/Privacy">Privacy</Link></p>
         </div>
     </footer>
     </React.Fragment>
